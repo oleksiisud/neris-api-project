1334af38833790c4ffb04a46b4ead9ed6890c024
Oleksii Sudarin <asudarin22@gmail.com>
2026-05-15 11:46:39 -0400

    Update CORS Middleware

--- api/main.py
+++ api/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import validate
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://neris-api-proj.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["POST", "GET", "OPTIONS"],
+    allow_headers=["Accept", "Content-Type"],
+)
 
 
 @app.get("/health")
